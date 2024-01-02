2134540eb52ce78f8d0e90d427c6bef9fc1f9cf4
Charmaine Lee <charmainelhm@gmail.com>
2024-01-02 20:21:20 +0800

    Set up generic styling for app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        charcoal: "var(--color-charcoal)",
+        indigo: "var(--color-indigo)",
+        neutral: {
+          100: "white",
+          800: "var(--color-charcoal)",
+        },
+        accent: "var(--color-indigo)",
+      },
+      fontFamily: {
+        bellefair: ["Bellefair", "serif"],
+        barlow: ["Barlow Condensed", "sans-serif"],
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("@tailwindcss/container-queries")],
+};
+export default config;
